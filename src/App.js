d7050b78ce39afd0930f53cd96cc8d9392c00b9c
Shahrzad <75183431+shahrzadblackfox@users.noreply.github.com>
2023-11-23 19:15:18 +0330

    adding style

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import {Gi3DGlasses}from "react-icons/gi"
+import SingelQuestion from "./Question";
+function App() {
+  return (
+    <div className="container"> 
+    <h3>سوالی برایتان پیش آمده؟</h3>
+    <div className="info">
+      <SingelQuestion />
+
+    </div>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Question.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { FaMinusCircle,  FaPlusCircle } from "react-icons/fa";
+
+function Question() {
+  return (
+    <div className="question">
+      <header>
+        <h4>HI</h4>
+        <button>< FaPlusCircle/></button>
+      </header>
+      <p>skdjgfhskdjhf</p>
+    </div>
+  );
+}
+
+export default Question;
